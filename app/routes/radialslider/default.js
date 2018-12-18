3ac935df803c0e7234c6bb370aaecc422e44817b
build <buildautomation@syncfusion.com>
2018-12-18 19:03:07 +0530

    Removed Old Samples

--- app/routes/radialslider/default.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-   model(){
-    return {
-            imageurl:"//js.syncfusion.com/demos/web/content/images/radialslider/chevron-right.png",
-        }
-    }
-});
