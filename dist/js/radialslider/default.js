acf591ce7bb8e5a55abe242a901063bb2b2f3ccf
BuildAutomation <buildautomation@syncfusion.com>
2021-08-11 13:42:16 +0530

    v19.2.0.55

--- dist/js/radialslider/default.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-   model(){
-    return {
-            imageurl:"//js.syncfusion.com/demos/jquery/content/images/radialslider/chevron-right.png",
-        }
-    }
-});
